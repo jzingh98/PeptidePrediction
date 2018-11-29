c8351b70245320b35902fafd74432f886ac31518
Bernie408 <32624251+Bernie408@users.noreply.github.com>
2018-11-28 23:47:34 -0800

    Setup details

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='PeptidePrediction',
+      version='0.0.1',
+      description='Peptide suprastructure prediction',
+      url='https://github.com/jzingh98/PeptidePrediction',
+      author='Jagveer Singh',
+      author_email='jzingh@ucdavis.edu',
+      license='MIT License',
+      packages=find_packages(),
+      zip_safe=False,
+      dependency_links=[]
+)
