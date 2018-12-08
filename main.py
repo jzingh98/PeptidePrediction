9778d7eef92900e869ec05d6517b96c5940061fb
Bernie408 <32624251+Bernie408@users.noreply.github.com>
2018-12-08 06:38:25 -0800

    Neural net initial implementation

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
+# Import Packages
 import pandas as pd
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation
+from keras.layers import Dense
 from keras.optimizers import SGD
-from keras.callbacks import History
-from keras.callbacks import ModelCheckpoint as mc
-import  string
-from ImportData import update
-import sklearn as sk
-from string import ascii_lowercase
 
 # ---- IMPORT DATA ----
 
 # Read CSV
 df = pd.read_csv("./RawData/Sample_Spreadsheet.csv")
 
-# Create Arrays
+# Create arrays to hold each column
 ar_Sequence = df.iloc[:, 0].values
 ar_Structure = df.iloc[:, 1].values
 ar_NTerminal = df.iloc[:, 2].values
@@ -24,7 +19,7 @@ ar_CTerminal = df.iloc[:, 3].values
 ar_NonTerminal = df.iloc[:, 4].values
 ar_CombinedX = df.iloc[:, 2:5].values
 
-# Encoding of structure and features
+# Encode the categorical columns
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelEncoder = LabelEncoder()
 encoded_Structure = labelEncoder.fit_transform(ar_Structure)
@@ -32,9 +27,9 @@ encoded_NTerminal = labelEncoder.fit_transform(ar_NTerminal)
 encoded_CTerminal = labelEncoder.fit_transform(ar_CTerminal)
 encoded_NonTerminal = labelEncoder.fit_transform(ar_NonTerminal)
 
-# Encoding of peptide sequence
-encoded_dfSequence = pd.DataFrame()
-alphabet_Sequence = np.array([])
+# Encoding peptide sequence
+encoded_dfSequence = pd.DataFrame()     # Not used yet
+alphabet_Sequence = np.array([])        # Used
 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = np.zeros((26, len(ar_Sequence)))
 count = 0
 for Line in ar_Sequence:
@@ -79,57 +74,62 @@ alphabet_Sequence = np.vstack((a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q
 #                                             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'))
 
 
-# Define train and test data
-X = np.vstack((alphabet_Sequence, encoded_NTerminal, encoded_CTerminal, encoded_NonTerminal))
+# Combine columns into single nd array
+X = np.vstack((alphabet_Sequence, encoded_NTerminal, encoded_CTerminal, encoded_NonTerminal)).transpose()
 Y = encoded_Structure
-print(X)
-print(" ")
-print(Y)
+Y_encoded = keras.utils.to_categorical(encoded_Structure, num_classes=12)    # Not used
 
 # Designate Train and Test Data
 x_train = np.vsplit(X, ([10, 20]))[0]
 x_test = np.vsplit(X, ([10, 20]))[1]
-y_train = np.vsplit(Y, ([10, 20]))[0]
+y_train = np.hsplit(Y, ([10, 20]))[0]
+y_test = np.hsplit(Y, ([10, 20]))[1]
+y_classnames, y_indices = np.unique(Y, return_inverse=True)
 y_train_classnames, y_train_indices = np.unique(y_train, return_inverse=True)
-y_train = keras.utils.to_categorical(y_train_indices, num_classes=10)
-y_test = np.vsplit(Y, ([10, 20]))[0]
 y_test_classnames, y_test_indices = np.unique(y_test, return_inverse=True)
-y_test = keras.utils.to_categorical(y_test_indices, num_classes=10)
+
+
+
+
+# Record data characteristics
+print(X.shape)
+print(x_train.shape)
+print(x_test.shape)
+print(Y.shape)
+print(y_train.shape)
+print(y_test.shape)
+print(y_classnames.shape)
+
+num_features = X.shape[1]
+num_classes = y_classnames.shape[0]
+print(num_features)
+print(num_classes)
+
+print(x_train)
+print(y_train)
+
 
 # Build Model
 model = Sequential()
-dense1 = model.add(Dense(3, activation='sigmoid', input_dim=8))
+dense1 = model.add(Dense(3, activation='sigmoid', input_dim=num_features))
 model.add(Dense(3, activation='sigmoid'))
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(num_classes, activation='softmax'))
 
 # Configure Model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
+model.compile(loss='sparse_categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-# Run Model
-official_history = History()
-# Format sample data
-x_sample = x_train[0].reshape(1, 8)
-y_sample = y_train[0].reshape(1, 10)
+# Training the ANN
+model.fit(x_train, y_train, batch_size=1, epochs=10)
 
-# Build model
-model = Sequential()
-model.add(Dense(3, activation='sigmoid', input_dim=8))
-model.add(Dense(3, activation='sigmoid'))
-model.add(Dense(10, activation='softmax'))
-sgd = SGD(lr=0.01)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# Predicting the Test set results
+y_pred = model.predict(x_test)
 
-# Run model
-fittedModel = model.fit(x_sample, y_sample, validation_data=(x_test, y_test), verbose=1, epochs=1, batch_size=1)
 
-# Print Gradient Descent Weights
+# Results
+print(y_pred)
+print(y_test)
 score = model.evaluate(x_test, y_test, batch_size=1)
-
-
-
-print(score)
+print("\nScore: " + str(score))
 
 
